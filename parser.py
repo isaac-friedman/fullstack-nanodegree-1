fb96cb29ebf2a6af2e3e6908be55fd8e4165f526
vagrant <vagrant@vagrant.vm>
2019-05-27 00:20:37 +0000

    feat: Added pre-implementation skeleton of the script

---
+++ parser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# A script that queries the news.sql database to answer very specific
+# questions.# This project will be submitted in partial fulfillment of the
+# requirements for Udacity's Fullstack Web Developer Nanodegree program.
+# Following that, it can be extended int a more general purpose log parser to
+# be adapted to other databases.
+
+
+import psycopg2
+
+DBNAME = "news"
+
+
+def top_three_articles():
+    print("Not yet implemented.")
+
+
+def top_authors():
+    print("Not yet implemented.")
+
+
+def high_error_days():
+    print("Not yet implemented.")
+
+
+def write_out(filename, content):
+    print("Not yet implemented.")
+
+
+if __name__ == '__main__':
+    top_three_articles()
+    top_authors()
+    high_error_days()
